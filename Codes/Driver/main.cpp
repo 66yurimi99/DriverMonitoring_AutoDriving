2be912bb956bc4486a60a4d704f748b4318dfb4d
jis0202 <141535958+jis0202@users.noreply.github.com>
2023-11-09 10:29:52 +0900

    Update main.cpp

--- Codes/Driver/main.cpp
+++ Codes/Driver/main.cpp
@@ -12,14 +12,12 @@ dlib::shape_predictor landmark_predictor;
 int frameCount = 1;
 int camera_num = 0; //내장 및 첫번째 카메라
 int gammaframe = 1;
-HINTERNET hInternet, hConnect; //인터넷 세션, 리소스 핸들
 
 int main() {
     _isp = new ISP();
-    _server = new Server();
+    _server = new Server(L"http://54.175.8.12/flag.php");
     _isp->initializeCamera(camera_num, cap);
     _isp->initializeDlib(detector, landmark_predictor);
-    _server->initalizeWininet(hInternet, hConnect);
     Mat frame;
     Mat dst; // image preprocessing
     Mat dst_hsv;
@@ -27,14 +25,17 @@ int main() {
     Mat gamma_t;
     Mat log_t;
 
+    wstring response;
+    
     bool sleep = false;
     bool pre_sleep = false;
     bool found = true;
     clock_t start = 0;
     clock_t end = 0;
     
-    
     auto start_fps = chrono::high_resolution_clock::now();
+
+    _server->sendGETRequest(L"?sleep=0", response);
     
     while (true) {
         if (!_isp->videotoframe(frame, cap)) break;
@@ -43,18 +44,24 @@ int main() {
         _isp->gammatransform(frame, gamma_t, gamma_var);
         _isp->detectEyesAndSleep(gamma_t, detector, landmark_predictor, sleep, start, end);
         _isp->calculateFPS(gamma_t, gammaframe, start_fps);
-        _server->request_Wininet_Get(hInternet, hConnect, sleep, pre_sleep);
+       
+        if (pre_sleep != sleep)
+        {
+           wstring query = L"?sleep=";
+           wstring value = sleep ? L"1" : L"0";
+           _server->sendGETRequest(query+value, response);
+           pre_sleep = sleep;
+        }
         
         cv::imshow("gamma trans", gamma_t);
         if (waitKey(1) == 27) {
             break;
         }
     }
-
-    InternetCloseHandle(hConnect);
-    InternetCloseHandle(hInternet);
+    
     destroyAllWindows();
     delete _isp;
+    delete _server;
 
     return 0;
 }
